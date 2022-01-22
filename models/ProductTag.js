9600c8ab68825a3bcf22c5210d6c303de95d93f0
bryance97 <bryancifuentesescobar@gmail.com>
2022-01-22 00:29:54 -0500

    updates

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -1,8 +1,8 @@
-const { Model, DataTypes } = require("sequelize");
+const { Model, DataTypes } = require('sequelize');
 
-const sequelize = require("../config/connection");
+const sequelize = require('../config/connection');
 
-class ProductTag extends Model {}
+class ProductTag extends Model { }
 
 ProductTag.init(
   {
@@ -11,29 +11,29 @@ ProductTag.init(
       type: DataTypes.INTEGER,
       allowNull: false,
       primaryKey: true,
-      autoIncrement: true,
+      autoIncrement: true
     },
     product_id: {
       type: DataTypes.INTEGER,
       references: {
-        model: "Product",
-        key: "id",
-      },
+        model: 'product',
+        key: 'id'
+      }
     },
     tag_id: {
       type: DataTypes.INTEGER,
       references: {
-        model: "Tag",
-        key: "id",
-      },
-    },
+        model: 'tag',
+        key: 'id'
+      }
+    }
   },
   {
     sequelize,
     timestamps: false,
     freezeTableName: true,
     underscored: true,
-    modelName: "product_tag",
+    modelName: 'product_tag',
   }
 );
 
